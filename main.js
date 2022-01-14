a3ccdf8764d6ab5ffeb559fe366ff513fbee232c
ameya2011 <79647483+ameya2011@users.noreply.github.com>
2022-01-14 13:48:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+nose_X=0
+nose_Y=0
+
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/mkLmqpgf/Untitled-drawing-2.png');
+}
+
+function setup(){
+canvas=createCanvas(300, 300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+posenet= ml5.poseNet(video, modelLoaded);
+posenet.on('pose', gotPoses);
+}
+
+
+
+function modelLoaded(){
+console.log('PoseNet is Initialised');
+}
+
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+nose_X=results[0].pose.nose.x-165;
+nose_Y=results[0].pose.nose.y-180;
+console.log("Nose X=" + nose_X);
+console.log("Nose Y=" + nose_Y);
+}
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clown_nose, nose_X, nose_Y , 440, 440);
+}
+function take_Snapshot(){
+save("MYfilter.png");
+}
+
+
+
+
